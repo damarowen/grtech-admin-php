62c259fe62fd2c5e0b216d3f158e366f92a6e8a6
Damar Oen Isyai <damarowen@gmail.com>
2026-03-19 22:57:11 +0700

    test

--- config/app.php
+++ config/app.php
@@ -7,7 +7,7 @@ return [
     | Application Name
     |--------------------------------------------------------------------------
     |
-    | This value is the name of your application, which will be used when the
+    | This value is the name of your application, which will be used when thee
     | framework needs to place the application's name in a notification or
     | other UI elements where an application name needs to be displayed.
     |
